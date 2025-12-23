463264c9f62871fdafc3c6c568d9bfcac5025d64
Kerem Sirin <kerem.sirin@commencis.com>
2025-12-24 02:36:55 +0300

    feat: add client display component

---
+++ src/components/layout/ClientDisplay/ClientDisplay.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import type { ReactElement } from 'react';
+import { Activity } from 'react';
+
+import type { ClientDisplayProps } from '@/components/layout/ClientDisplay/ClientDisplay.types';
+
+import { Breakpoint, useMediaQuery } from '@/hooks';
+
+export const ClientDisplay = ({
+  children,
+  showOnSm,
+  showOnMd,
+  showOnLg,
+}: ClientDisplayProps): ReactElement => {
+  const isSm = useMediaQuery({ down: Breakpoint.Md });
+  const isMd = useMediaQuery({
+    between: { min: Breakpoint.Md, max: Breakpoint.Lg },
+  });
+  const isLg = useMediaQuery({ up: Breakpoint.Lg });
+  const hasMediaQueryValues = isSm !== null || isMd !== null || isLg !== null;
+
+  const isVisible = hasMediaQueryValues
+    ? (showOnSm && isSm) || (showOnMd && isMd) || (showOnLg && isLg)
+    : showOnSm;
+
+  return (
+    <Activity mode={isVisible ? 'visible' : 'hidden'}>{children}</Activity>
+  );
+};
+
+export default ClientDisplay;
