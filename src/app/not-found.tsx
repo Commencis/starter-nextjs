e583899a7e4b27d78f7088c9fc84cdb4732182cc
Mehmetcan Yılmaz <github@mcan.me>
2026-02-05 14:24:50 +0300

    fix: error component path

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@
 
 import type { ReactElement } from 'react';
 
-import { Error404 } from '@/components/error';
+import { Error404 } from '@/components/shared';
 
 export default function NotFoundPage(): ReactElement {
   return <Error404 />;
