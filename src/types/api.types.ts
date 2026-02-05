d252671e4cd5198ca9ac097087c3beb98b2443ca
Mehmetcan Yılmaz <github@mcan.me>
2026-02-05 14:28:08 +0300

    chore: remove commencis copyright

--- src/types/api.types.ts
+++ src/types/api.types.ts
@@ -1,12 +1,3 @@
-/*
- * Copyright 2017-2025 Commencis. All Rights Reserved.
- *
- * Save to the extent permitted by law, you may not use, copy, modify,
- * distribute or create derivative works of this material or any part
- * of it without the prior written consent of Commencis.
- * Any reproduction of this material must contain this notice.
- */
-
 export const enum HttpStatusCode {
   Ok = 200,
   NoContent = 204,
