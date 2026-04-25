deb2942b22966d01f3fdcc968acf65577e6556c5
Mehmetcan Yılmaz <github@mcan.me>
2026-04-26 01:19:47 +0300

    chore: remove container from homepage

--- src/modules/home/Home.page.tsx
+++ src/modules/home/Home.page.tsx
@@ -1,6 +1,5 @@
 import type { ReactElement } from 'react';
 
-import { Container } from '@/components/layout';
 import { Box, Button, Text } from '@/components/ui';
 
 export function HomePage(): ReactElement {
@@ -13,18 +12,16 @@ export function HomePage(): ReactElement {
       height="full"
       gap={12}
     >
-      <Container>
-        <Text as="h1" variant="body-2">
-          Welcome to the Commencis Starter
-        </Text>
-        <Text as="h1" variant="heading-2">
-          Next.js Template
-        </Text>
+      <Text as="h1" variant="body-2">
+        Welcome to the Commencis Starter
+      </Text>
+      <Text as="h1" variant="heading-2">
+        Next.js Template
+      </Text>
 
-        <Button variant="primary" size="md">
-          Start Building
-        </Button>
-      </Container>
+      <Button variant="primary" size="md">
+        Start Building
+      </Button>
     </Box>
   );
 }
